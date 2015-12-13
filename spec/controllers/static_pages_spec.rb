16297bc3ea0c1b780cc9886d108e27603ed243da
clodny <stasrelide@gmail.com>
2015-12-13 22:41:19 +0200

    develop_LVLET-41: Add factory girl gem and rewrite tests

--- spec/controllers/static_pages_spec.rb
+++ spec/controllers/static_pages_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController do
-  describe 'GET #index' do
-    subject { get :index }
-    it 'renders the index template' do
-    	@x = [{blabla: 'bla'}, {blabla:'blabla'}].to_json
-      expect(subject.body).to eq(@x)
-
-RSpec.describe StaticPagesController do
   describe "GET #index" do
     subject { get :index }
 
