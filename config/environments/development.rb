b325887308d7ba442f808a96ea380ff3d991cb32
tanyastakh <tanyastakh26@gmail.com>
2015-12-16 23:24:00 +0200

    LVLET-13: Refactoring according to style-guide

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Setting up default url for mailer action
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   #Adding and configing delivery method(smtp)
   config.action_mailer.delivery_method = :smtp
