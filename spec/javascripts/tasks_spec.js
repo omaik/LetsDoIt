b75039414ac4e7e2cd5b17bdbdd11ef4b8120bd1
Yevgenii Malyshenko <e.malyshenko@gmail.com>
2016-01-04 15:17:53 +0200

    LVLET-57 develop Cover Tasks with unit tests (Angular)

--- spec/javascripts/tasks_spec.js
+++ spec/javascripts/tasks_spec.js
@@ -1,5 +1,151 @@
-describe('A suite', function() {
-  it('contains spec with an expectation', function() {
-    expect(true).toBe(true);
+describe('Tasks tests', function() {
+
+
+  describe('TasksListController', function() {
+
+    var scope, TasksListController, tasks, priorities, resource;
+    tasks = [
+      { id: 1, name: 'task 1', priority_id: '1', status: '2', description: 'description 1' },
+      { id: 2, name: 'task 2', priority_id: '1', status: '2', description: 'description 2' }
+    ];
+    priorities = [
+      { name: 'priority', value: 1, color:'#ffffff', id: 1 }
+    ];
+
+    beforeEach(function() {
+      module('letsDoIt');
+      inject(function(
+        _$controller_,
+        _$httpBackend_,
+        _tasksResource_,
+        _prioritiesResource_,
+        $rootScope,
+        $resource) {
+          $httpBackend = _$httpBackend_;
+          tasksResource = _tasksResource_;
+          prioritiesResource = _prioritiesResource_;
+          $controller = _$controller_;
+          scope = $rootScope.$new();
+          resource = $resource;
+          TasksListController = $controller('TasksListController', { $scope: scope, tasksResource: tasksResource });
+      });
+    });
+
+     afterEach(function() {
+       $httpBackend.verifyNoOutstandingExpectation();
+       $httpBackend.verifyNoOutstandingRequest();
+     });
+
+    it('should get tasks list', function() {
+      $httpBackend.whenGET('/tasks.json').respond(tasks);
+      $httpBackend.whenGET('/priorities.json').respond(priorities);
+      tasksResource.query(function(data) {
+        scope.tasks.list = data;
+      });
+      $httpBackend.flush();
+      expect(JSON.stringify(scope.tasks.list)).toEqual(JSON.stringify(tasks));
+    });
+
+      it('should create new task', function() {
+        var task;
+        scope.task = {
+          name: 'Task 3',
+          priority_id: '3',
+          status: '2'
+        };
+        task = scope.task;
+        $httpBackend.whenPOST('/tasks.json').respond(201);
+        $httpBackend.whenGET('/priorities.json').respond(priorities);
+        $httpBackend.whenGET('/tasks.json').respond(tasks);
+        tasksResource.query(function(data) {
+          scope.tasks.list = data;
+        });
+        spyOn(tasksResource, 'query').and.callFake(function() {
+          scope.tasks.list.push(task);
+          return scope.tasks.list;
+        });
+        scope.addTask();
+        $httpBackend.flush();
+        expect(JSON.stringify(scope.tasks.list)).toContain(JSON.stringify(task));
+      });
+
+        it('should not create task with blank name', function() {
+          var task;
+          scope.task = {
+            name: '',
+            priority_id: '3',
+            status: '2'
+          };
+          task = scope.task;
+          $httpBackend.whenPOST('/tasks.json').respond(201);
+          $httpBackend.whenGET('/priorities.json').respond(priorities);
+          $httpBackend.whenGET('/tasks.json').respond(tasks);
+          tasksResource.query(function(data) {
+            scope.tasks.list = data;
+          });
+          spyOn(tasksResource, 'query').and.callFake(function() {
+            scope.tasks.list.push(task);
+            return scope.tasks.list;
+          });
+          scope.addTask();
+          $httpBackend.flush();
+          expect(JSON.stringify(scope.tasks.list)).not.toContain(JSON.stringify(task));
+      });
+
+    it('should delete task', function() {
+      var task = new tasksResource(tasks[1]);
+      $httpBackend.whenDELETE('/tasks/' + task.id + '.json').respond(204);
+      $httpBackend.whenGET('/tasks.json').respond(scope.tasks.list);
+      $httpBackend.whenGET('/priorities.json').respond(priorities);
+      scope.deleteTask(task);
+      $httpBackend.flush();
+      expect(JSON.stringify(scope.tasks.list)).not.toContain(JSON.stringify(task));
+    });
+  });
+
+  describe('EditTaskController', function() {
+    var scope, EditTaskController, priorities, resource;
+    tasks = [
+      { id: 1, name: 'task 1', priority_id: '1', status: '2', description: 'description 1' },
+      { id: 2, name: 'task 2', priority_id: '1', status: '2', description: 'description 2' }
+    ];
+    priorities = [
+      { name: 'priority', value: 1, color:'#ffffff', id: 1 }
+    ];
+
+    beforeEach(function() {
+      module('letsDoIt');
+      inject(function(
+        _$controller_,
+        _$httpBackend_,
+        _tasksResource_,
+        _prioritiesResource_,
+        $rootScope,
+        $resource) {
+          $httpBackend = _$httpBackend_;
+          tasksResource = _tasksResource_;
+          prioritiesResource = _prioritiesResource_;
+          $controller = _$controller_;
+          scope = $rootScope.$new();
+          resource = $resource;
+          EditTaskController = $controller('EditTaskController', { $scope: scope, tasksResource: tasksResource });
+      });
+    });
+
+     afterEach(function() {
+       $httpBackend.verifyNoOutstandingExpectation();
+       $httpBackend.verifyNoOutstandingRequest();
+     });
+
+    it('should update task', function() {
+      var task = new tasksResource(tasks[1]);
+      task.description = 'new description';
+      $httpBackend.whenPUT(task.id + '.json').respond(204);
+      $httpBackend.whenGET('/tasks.json').respond(task);
+      $httpBackend.whenGET('/priorities.json').respond(priorities);
+      scope.updateTask();
+      $httpBackend.flush();
+      expect(task.description).toMatch('new description');
+    });
   });
 });
