f4264926516dd06407d21ff227c047d6f187183e
tanyastakh <tanyastakh26@gmail.com>
2016-01-18 20:10:16 +0200

    develop_LVLET-124: Additional features for profile

--- app/assets/javascripts/controllers/main/profiles/editProfilesController.js
+++ app/assets/javascripts/controllers/main/profiles/editProfilesController.js
@@ -7,8 +7,12 @@ angular.module('letsDoIt')
   'userProfile',
   'Auth',
   'Upload',
- function($scope, $location, $state, $stateParams, userProfile, Auth, Upload) {
-  $scope.currentUser = Auth._currentUser;
+  '$timeout',
+ function($scope, $location, $state, $stateParams, userProfile, Auth, Upload, $timeout) {
+  $scope.image = {
+    myImage: '',
+    croppedImage: {}
+  }
   $scope.day = [];
   for (var i = 0; i <= 30; ++i) {
     $scope.day.push(i+1);
@@ -18,10 +22,10 @@ angular.module('letsDoIt')
     $scope.month.push(i+1);
   };
   $scope.year = [];
-  for (var i = 1979; i <= 2009; ++i) {
+  for (var i = 2009; i >= 1929; --i) {
     $scope.year.push(i+1);
   };
-  $scope.namePattern = /^[a-zA-Z\u0400-\u04FF]+$/;
+  $scope.formatPattern = /^[a-zA-Z\u0400-\u04FF]+$/;
   $scope.errorMessages = {
     'pattern':'has incorrect format',
     'maxlength':'is too long'
@@ -29,34 +33,42 @@ angular.module('letsDoIt')
   $scope.err = {
       errors: {},
       isError: false
-    }
-  $scope.profile = userProfile.get({ id: $stateParams.id }, function() {
-  });
-  $scope.updateProfile = function(file) {
-    var profileId = $scope.profile.id;
-    var resource_url = '/users/'+ $scope.profile.id +'.json';
-     if (file !== undefined) {
-        file.upload = Upload.upload({
-            url: resource_url,
-            method: 'PUT',
-            fields: { 'user[avatar]': $scope.avatar },
-            file: file,
-            fileFormDataName: 'user[avatar]'
-        });
-    }
-    var profileId = $scope.profile.id;
-    var NOT_VALID_NAME = "You entered invalid data";
-    if($scope.editProfile.$invalid) {
-      $scope.errHandle = true;
-      $scope.profileErrMsg = NOT_VALID_NAME;
-      return;
+  };
+  function handleFileSelect(evt) {
+    var file = evt.currentTarget.files[0],
+    reader = new FileReader();
+    reader.onload = function (evt) {
+      $scope.$apply(function($scope){
+        $scope.image.myImage = evt.target.result;
+      });
     };
-    $scope.profile.$update(function() {
+    reader.readAsDataURL(file);
+  };
+  angular.element(document.querySelector('#fileInput')).on('change',handleFileSelect);
+  $scope.profile = userProfile.get({ id: $stateParams.id });
+  $scope.updateProfile = function(file) {
+    var profileId = $scope.profile.id,
+    resource_url = '/users/'+ $scope.profile.id +'.json';
+    if ($scope.avatar) {
+      file.upload = Upload.upload({
+        url: resource_url,
+        method: 'PUT',
+        fields: { 'user[avatar]': $scope.image.croppedImage, 'user[first_name]': $scope.profile.first_name, 'user[last_name]': $scope.profile.last_name, 'user[sex]': $scope.profile.sex, 'user[day]': $scope.profile.day, 'user[month]': $scope.profile.month, 'user[year]': $scope.profile.year, 'user[country]': $scope.profile.country, 'user[city]': $scope.profile.city},
+        file: file,
+        fileFormDataName: 'user[avatar]'
+      });
+      $timeout(function() {
+        $state.go('profile', {id: profileId}), 1;
+      });
 
+    }
+    else{
+      $scope.profile.$update(function() {
       $state.go('profile', {id: profileId});
     },
     function(error, status) {
       console.log('Status is: ' + status);
     });
+    };
   };
 }]);
