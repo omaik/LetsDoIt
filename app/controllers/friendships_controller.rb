2a8ec95ae65d7fd30a270e728df6ba72419727d3
Ihor Voytovych <asyberio@gmail.com>
2016-01-23 14:35:21 +0000

    LVLET-63: Rspec tests for Friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,33 +1,31 @@
 class FriendshipsController < ApplicationController
+  respond_to :json
   before_action :authenticate_user!
     
   def index
-    render json: {relations:{friends: current_user.friends, pending: current_user.pending_friends, requested: current_user.requested_friends}}
+    respond_with ({friends: current_user.friends, pending: current_user.pending_friends, requested: current_user.requested_friends})
   end
   
   # Friendship request action
   def create
-      if params[:friend_id]
-        @friend = User.find_by(id: params[:friend_id])
-        @friendship = Friendship.request(current_user, @friend)
-        render json: {status:"ok", message: "Success!"}
-      end
+    if params[:friend_id]
+      friend = User.find_by(id: params[:friend_id])
+      Friendship.request(current_user, friend)
+      respond_with friend
+    end
   end
   
   # Accept friendship request action
   def accept
-    @friend = User.find_by(id: params[:id])
-    @user_friendship = current_user.friendships.where(friend_id: @friend.id).first
-    if @user_friendship.accept!
-      render json: {status:"ok", message: "Success!"}
-    end
+    friend = User.find_by(id: params[:id])
+    user_friendship = current_user.friendships.where(friend_id: friend.id).first
+    user_friendship.accept!
+    respond_with  user_friendship
   end
   
   def destroy
-    @active_friendsdhip = current_user.friendships.where(friend_id: params[:id]).first
-    @active_friendsdhip.destroy 
-    render json: {status:"ok", message: "Success!"}
+    active_friendsdhip = current_user.friendships.where(friend_id: params[:id]).first
+    respond_with active_friendsdhip.destroy 
   end
-  
 end
 
