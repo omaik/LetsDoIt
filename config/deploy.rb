08d80041ec9de30d1d9434d272b5ace3c0d48ddb
OlehMaik <persualiti@gmail.com>
2016-02-09 13:20:25 +0200

    Revert "Merge branch 'develop_LVLET-96' into 'develop'"
    This reverts commit 31355970d52488335bbd2e9f88caa7dc7d303c17, reversing
    changes made to 519df76ebde40fd260ce94b1243e7132a5a53b4b.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+require 'capistrano/rails'
+require 'capistrano/bundler'
+require 'capistrano/rvm'
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
+require 'capistrano/ssh_doctor'
+
 set :application, 'LetsDoIt'
 set :repo_url, 'git@gitlab.com:rtriska/LetsDoIt.git'
 set :deploy_to, '/www/LetsDoIt'
@@ -5,13 +14,37 @@ set :scm, :git
 set :branch, 'develop'
 set :user, 'ubuntu'
 set :use_sudo, false
+set :rails_env, 'production'
 set :deploy_via, :remote_cache
 set :keep_releases, 2
 set :pty, true
-set :unicorn_rack_env, -> { 'production' }
-set :rails_env, 'production'
+
+server "54.175.169.251", :roles => [:app, :web, :db], :primary => true
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+
+  desc 'Symlink shared config files'
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+  end
+
+    desc 'Precompile assets after deploy'
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+
+  desc 'Restart applicaiton'
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
 end
+
+after 'deploy', 'deploy:symlink_config_files'
+after 'deploy', 'deploy:restart'
+after 'deploy', 'deploy:precompile_assets'
+
