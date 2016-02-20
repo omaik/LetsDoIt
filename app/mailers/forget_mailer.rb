6bf462c1d532c094d756d7a1335b002553a11f8d
Andriy Savinov <andriysavinof@gmail.com>
2016-02-20 16:33:11 +0200

    --no-edit

--- app/mailers/forget_mailer.rb
+++ app/mailers/forget_mailer.rb
@@ -3,8 +3,13 @@ class ForgetMailer < ApplicationMailer
   default from: ENV['GMAIL_USERNAME']
 
   def reset_password_instructions(password, user)
+    I18n.locale = user.language
     @password = password
     @user = user
+<<<<<<< HEAD
     mail(to: @user.email, subject: I18n.t('reset_password_LetsDoIt'))
+=======
+    mail(to: @user.email, subject: "#{I18n.t('reset_password_LetsDoIt')}")
+>>>>>>> --no-edit
   end
 end
